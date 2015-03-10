a516dfc9de80277d0229c12355be8f0194f6a0d3
YueKang17 <yue.kang1994@gmail.com>
2015-03-09 20:33:34 -0700

    Project Space shooter: added scripts for destroying asteriod.

---
+++ Space-shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+
+		//Debug.Log (other.name);
+		//marks objects to be destroyed at end of frame
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+
+		//end of frame, destroys objects
+	}
+}
---
+++ Space-shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	void Start ()
+	{
+		//takes tumble value and set angular velocity.
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
