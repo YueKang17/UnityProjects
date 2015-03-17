66905aa422e8114e648a9de0b4334e3e82419980
YueKang17 <yue.kang1994@gmail.com>
2015-03-16 20:16:34 -0700

    Project Space shooter, can now display and count score values

--- Space-shooter/Assets/Scripts/DestroyByContact.cs
+++ Space-shooter/Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,21 @@ public class DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	private GameController gameController;
+	public int scoreValue;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+		if (gameController == null) 
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
 
 	void OnTriggerEnter(Collider other) 
 	{
@@ -18,6 +33,7 @@ public class DestroyByContact : MonoBehaviour
 		{
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		}
+		gameController.AddScore (scoreValue);
 		Destroy(other.gameObject);
 		Destroy (gameObject);
 	}
